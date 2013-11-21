4f017307220c8b5e1d1a663b13dc76bef8dd540b
Sean Winn <sean.winn@cloudscaling.com>
2013-11-21 12:45:37 -0800

    updated ssh commands echo'd to user

--- sdn.sh
+++ sdn.sh
@@ -2,19 +2,7 @@
 
 MY_IP=$(ifconfig eth1 | awk '/inet addr/ {split ($2,A,":"); print A[2]}')
 
-# Install a desktop
-sudo apt-get -y no-install-recommends install ubuntu-desktop wireshark
 
-# Install Mininet
-sudo apt-get -y install git-core openssh-server
-git clone git://github.com/mininet/mininet /home/vagrant/mininet
-cd /home/vagrant/mininet
-cd
-time /home/vagrant/mininet/util/install.sh
-
-sudo mn --test pingall
-
-sudo /etc/init.d/openvswitch-controller stop
 
 # Install Open Daylight
 apt-get install -y maven git openjdk-7-jre openjdk-7-jdk unzip
@@ -29,7 +17,5 @@ echo "ssh -XY ${MY_IP}"
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64"
 echo "bash /home/vagrant/opendaylight/run.sh"
 echo
-echo "mn --controller=remote,ip=${MY_IP} --topo=linear,2 --listenport=6633"
-echo "sudo wireshark"
 echo "--------------------------------"
 echo "You can also browse to http://${MY_IP}:8080"
